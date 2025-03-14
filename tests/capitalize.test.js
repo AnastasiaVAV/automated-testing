318de3a53addac12e64deabf601743c4a6685a41
nastya volkova <derric41@mail.ru>
2025-03-14 18:23:39 +0400

    fix capitlise.test.js: add assert

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,11 +1,16 @@
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-if (capitalize('hello') !== 'Hello') {
-  throw new Error('Функция работает неверно!');
-}
+// первый вариант
+// if (capitalize('hello') !== 'Hello') {
+//   throw new Error('Функция работает неверно!');
+// }
+// if (capitalize('') !== '') {
+//   throw new Error('Функция работает неверно!');
+// }
 
-if (capitalize('') !== '') {
-  throw new Error('Функция работает неверно!');
-}
+// use assert
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
 
 console.log('Все тесты пройдены!');
